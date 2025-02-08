684288fcc5254fb7e7ca1104c74ab85aab1fcc84
Rjk-Jami <rjkhan.1221@gmail.com>
2025-02-08 13:14:00 +0600

    fix mongo

--- Database/ConnectDatabase.js
+++ Database/ConnectDatabase.js
@@ -6,10 +6,7 @@ const connectDatabase = async () => {
     if (!dbUri) {
       throw new Error("MongoDB URI not found in environment variables");
     }
-    await mongoose.connect(dbUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(dbUri);
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection failed:", error);
